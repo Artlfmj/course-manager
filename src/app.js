fe56dec3e79c193126afdd01015eccd7746fb6f1
Khuzaima Nawab <63063076+KhuzaimaNawab@users.noreply.github.com>
2023-10-04 21:58:55 +0500

    feat(submission): Added button that allows to upload files

--- src/app.js
+++ src/app.js
@@ -13,11 +13,14 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const dotenv = require("dotenv");
 const path = require("path");
+const multer = require("multer");
 
 const dbConfig = require("./config/dbconfig");
-dotenv.config();
+dotenv.config({ path: "./config.env" });
 // Connect to MongoDB using the configuration
 dbConfig();
+
+dotenv.config();
 
 const courseModel = require("./db/courseDB");
 
@@ -60,7 +63,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 //changes
 const csrfProtection = csrf({ cookie: true });
-app.use(csrfProtection);
+// app.use(csrfProtection);
 
 app.get("/login", limiter, csrfProtection, (req, res) => {
   if (req.isAuthenticated()) {
@@ -233,6 +236,66 @@ app.post("/search-course", limiter, isAuthenticated, async function (req, res) {
     res.json({ message: "An error occurred while searching." });
   }
 });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads"); //Make a folder named uploads otherwise it will fail
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.use(
+  "/submission",
+  limiter,
+  isAuthenticated,
+  csrfProtection,
+  async function (req, res) {
+    return res.render("submission");
+  }
+);
+
+app.post(
+  "/upload",
+  limiter,
+  isAuthenticated,
+  csrfProtection,
+  upload.array("files"),
+  async function (req, res) {
+    console.log("api");
+    const errors = [];
+    const maxSize = 15 * 1024 * 1024; //15Mb file size limit
+    const allowedFileTypes = [
+      ".doc",
+      ".docx",
+      ".xls",
+      ".xlsx",
+      ".ppt",
+      ".pptx",
+      ".txt",
+      //add or remove allowed files type
+    ];
+    if (req.files.length <= 0) {
+      return res.render("submission", { error: "No File found" });
+    }
+    req.files.forEach((file) => {
+      const fileExtension = path.extname(file.originalname).toLowerCase();
+      if (!allowedFileTypes.includes(fileExtension)) {
+        errors.push(`Invalid file type: ${file.originalname}`);
+      }
+      if (file.size > maxSize) {
+        errors.push(`${file.originalname} size is greater then 15MB`);
+      }
+    });
+    if (errors.length > 0) {
+      return res.render("submission", { error: errors });
+    } else {
+      return res.render("submission", { message: "File submit succesfully" });
+    }
+  }
+);
 
 app.use("/css", express.static("src/css"));
 
